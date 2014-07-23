7511c48cd8843a24fba59e302efd9bd0011492ce
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-22 22:16:51 -0400

    changed to use ng-repeat

--- mealCalculator.js
+++ mealCalculator.js
@@ -18,8 +18,23 @@ angular.module('mealCalculator',[])
 
 			$scope.inputs={
 
-				taxRate: 8.5,
-				tipPercentage: 17.0
+				basePrice:{
+					label: "Base Meal Price: $",
+					value:undefined,
+					index: 1
+				},
+
+				taxRate:{
+					label:"Tax Rate: %",
+					value:8.5,
+					index: 2
+				},
+
+				tipPercentage:{
+					label:"Tip Percentage: %",
+					value:17.0,
+					index: 3
+				}
 
 			};
 
@@ -51,11 +66,13 @@ angular.module('mealCalculator',[])
 
 		};
 
-		$scope.$watchCollection('inputs',function(newVal,oldVal){
+		$scope.$watch('inputs',function(newVal,oldVal){
 
 			$rootScope.$broadcast('mdEdit',$scope.inputs);
 
-		});
+			console.log(newVal);
+
+		},true);
 
 	})
 
@@ -65,9 +82,9 @@ angular.module('mealCalculator',[])
 
 		$scope.$on('mdEdit',function(event,inputs){
 
-			$scope.inputs.subTotal = inputs.basePrice + inputs.basePrice * inputs.taxRate / 100;
+			$scope.inputs.subTotal = inputs.basePrice.value + inputs.basePrice.value * inputs.taxRate.value / 100;
 
-			$scope.inputs.tip = inputs.basePrice * inputs.tipPercentage / 100;
+			$scope.inputs.tip = inputs.basePrice.value * inputs.tipPercentage.value / 100;
 
 			$scope.inputs.total = $scope.inputs.subTotal + $scope.inputs.tip;
 
@@ -99,12 +116,38 @@ angular.module('mealCalculator',[])
 
 		$scope.$on('mdSubmit',function(event,inputs){
 
-			$scope.earnings.tipTotal += inputs.basePrice * inputs.tipPercentage / 100;
+			$scope.earnings.tipTotal += inputs.basePrice.value * inputs.tipPercentage.value / 100;
 
 			$scope.earnings.mealCount++;
 
 			$scope.earnings.tipAverage = $scope.earnings.tipTotal /  $scope.earnings.mealCount;
 
 		})
+
+	})
+
+	.filter('orderObjectBy', function() {
+	  
+	  return function(items, field, reverse) {
+
+	    var filtered = [];
+
+	    angular.forEach(items, function(item) {
+
+	      filtered.push(item);
+	    
+	    });
+	    
+	    filtered.sort(function (a, b) {
+	    
+	      return (a[field] > b[field] ? 1 : -1);
+	    
+	    });
+	    
+	    if(reverse) filtered.reverse();
+	    
+	    return filtered;
+	  
+	  };
 
 	});
